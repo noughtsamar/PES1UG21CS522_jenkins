674dac6fe075445311ab2816231d866c6a998c35
definitelynotsam27 <92373593+noughtsamar@users.noreply.github.com>
2024-03-06 13:25:45 +0530

    change

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout<< "Hello PES1UG21CS522\n";
+  cout<< "Hello PES1UG21CS522 changed\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
